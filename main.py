e57ddefdd0d0b260748cf47c91ec765789626878
Patrick Andersen <ulf.patrick.bertil@gmail.com>
2024-12-11 21:39:22 -0500

    minor stuff

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def main():
     wifi = MyWifi.create_instance_from_settings()
     door = Door.create_instance_from_settings()
     print(f"Door state before starting listener: {DoorState.state_string(door.current_state)}")
-    doorCmdListener = DoorCommandListener(door, wifi, start_listening=True)
+    doorCmdListener = DoorCommandListener(door, wifi, start_listening=True,  print_to_console=True)
 
 main()
